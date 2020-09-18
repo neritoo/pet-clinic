279b88cb8fa82208fa6751673d419255d019bffc
neritoo <ezegavilan95@gmail.com>
2020-09-18 01:55:10 -0300

    Added contact info for Owner.

--- pet-clinic-data/src/main/java/com/gavilan/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/gavilan/petclinic/model/Specialty.java
@@ -1,5 +1,9 @@
 package com.gavilan.petclinic.model;
 
+/**
+ * Clase que representa una especialidad de un veterinario.
+ */
+
 public class Specialty extends BaseEntity {
 
     private String description;
