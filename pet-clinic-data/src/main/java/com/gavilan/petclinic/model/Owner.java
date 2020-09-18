967d5a643cbd01b98d2f1b2c03119cbb8b748791
neritoo <ezegavilan95@gmail.com>
2020-09-18 01:32:45 -0300

    Adding Owner and Visit Entities. Closes #25.

--- pet-clinic-data/src/main/java/com/gavilan/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/gavilan/petclinic/model/Owner.java
@@ -1,7 +1,27 @@
 package com.gavilan.petclinic.model;
 
+import java.util.Set;
+
 /** Clase que representa a un dueño, el cual querrá programar visitas para su mascota.
  * @author Ezequiel Gavilán
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Owner() {
+
+    }
+
+    public Owner(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
